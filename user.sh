80006144335ccddd9bf41a0e1b2f6b75948797bf
Keerthana <s4csb@debian-9>
2017-03-21 16:01:53 +0530

    added

---
+++ user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "currently logged user:$(echo $USER)"
+ec
+echo "current shell:$(echo $SHELL) "
+echo "home directory:$( echo $HOME)"
+echo "current workig directory: $(echo $PWD)"
+echo "operating system:$ (echo $(uname -a)
