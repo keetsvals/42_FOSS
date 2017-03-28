d10ddf2486eaaaf7158d64f59e019deec4d32416
Keerthana <s4csb@debian-9>
2017-03-28 15:48:52 +0530

    hi

---
+++ array.pl
@@ -0,0 +1,12 @@
+print"enter the array size:";
+$n =<STDIN>;
+for($i=0;$i < $n ; $i++)
+{
+ print"enter the elements :";
+ $array[$i] =<STDIN>;
+}
+print "the array is :\n";
+for ($i=0;$i<$n;$i++)
+{
+ print "$array[$i] \n";
+}
