23e4340012b1ec0503a053939996fd0493b630ef
Keerthana <s4csb@debian-9>
2017-03-28 15:57:14 +0530

    hi

---
+++ add.pl
@@ -0,0 +1,6 @@
+print "enetr a ";
+$a=<STDIN>;
+print "enter b" ;
+$b=<STDIN>;
+$sum=$a +$b ;
+print "the sum of$a and$b is $sum \n";
---
+++ fact.pl
@@ -0,0 +1,9 @@
+print "enter the no for factorial:";
+$n=<STDIN>;
+$fact=1;
+for($i=1;$i<=$n;$i++)
+{
+$fact = $fact * $i ;
+ 
+}
+print "the factorial of no $n : $fact \n";
